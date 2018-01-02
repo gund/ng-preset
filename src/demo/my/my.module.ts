45a0992acc4f777bd3afc0c2997327555d573f4c
Alex Malkevich <oleksii.malkevych@emaratech.ae>
2018-01-02 17:30:03 +0400

    fix(preset): Make sure AOT will be able to collect metadata
    And also accept preseted component so further operations can be possible (like custom decorators)
    
    BREAKING CHANGE: Now module import module instead of `.withPreset` -> `.forComponent` by passing
    component to apply preset on as the first argument and preset component itself as second argument

--- src/demo/my/my.module.ts
+++ src/demo/my/my.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { PresetDefaultModule } from '../../preset';
 import { MyComponentModule } from './my-component.module';
 import { MyPresetDefaultComponent } from './my-preset-default/my-preset-default.component';
+import { MyComponent } from './my.component';
 
 @NgModule({
   imports: [
     MyComponentModule,
-    PresetDefaultModule.withPreset(MyPresetDefaultComponent),
+    PresetDefaultModule
+      .forComponent(MyComponent, MyPresetDefaultComponent),
   ],
   exports: [MyComponentModule],
   declarations: [MyPresetDefaultComponent],

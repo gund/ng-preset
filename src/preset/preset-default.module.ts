8b2092b409df057a7c383636110c5f8c1f2c4c30
gund <malkevich.alex@gmail.com>
2018-01-11 01:33:35 +0400

    fix(preset): Fix types of preset input in modules
    And add structural difference to the abstrac preset type so only those who extend from it will  be
    allowed

--- src/preset/preset-default.module.ts
+++ src/preset/preset-default.module.ts
@@ -1,9 +1,9 @@
-import { PRESET_COMPS_TOKEN } from './preset-token';
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule, Type } from '@angular/core';
 
-import { providePresetFor } from './provide/preset-for';
+import { PRESET_COMPS_TOKEN, PresetType } from './preset-token';
 import { PresetService } from './preset.service';
+import { providePresetFor } from './provide/preset-for';
 
 @NgModule({
   imports: [CommonModule],
@@ -14,14 +14,14 @@ import { PresetService } from './preset.service';
 export class PresetDefaultModule {
   static forComponent(
     component: Type<any>,
-    presetType: Type<any>,
+    presetType: Type<PresetType>,
   ): ModuleWithProviders {
     return PresetDefaultModule.forComponents([component], presetType);
   }
 
   static forComponents(
     components: Type<any>[],
-    presetType: Type<any>,
+    presetType: Type<PresetType>,
   ): ModuleWithProviders {
     return providePresetFor(PresetDefaultModule, presetType, [
       { provide: PRESET_COMPS_TOKEN, useValue: components, multi: true },
